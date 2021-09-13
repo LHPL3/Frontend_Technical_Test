6fea29c91f8eef71b9d114a11a1b9a43660a582a
Aaron <aaronbritt12@googlemail.com>
2021-09-13 09:33:10 +0100

    edit movie function

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -41,6 +41,47 @@ export default class MovieListItem extends Component {
     }
   }
 
+  @action async editMovie(event) {
+    try {
+      const db = getFirestore();
+      const moviesRef = collection(db, 'movies');
+      const moviesSnapshot = await getDocs(moviesRef);
+      const movies = [];
+
+      moviesSnapshot.forEach((doc) => movies.push(doc));
+
+      this.movies = movies;
+
+      let toEdit = '';
+      movies.forEach(async (movie) => {
+        if (
+          movie._document.data.value.mapValue.fields.title.stringValue ===
+          this.movie.title
+        ) {
+          toEdit = movie.ref;
+        }
+      });
+
+      const target = event.target.id;
+      const result = window.prompt(`New ${target}`);
+
+      if (target === 'rating' && result >= 0 && result <= 5) {
+        const edit = { [`${target}`]: result };
+        await updateDoc(toEdit, edit);
+        window.location.reload(true);
+      } else if (
+        (target === 'description' || target === 'title') &&
+        result.length > 0
+      ) {
+        const edit = { [`${target}`]: result };
+        await updateDoc(toEdit, edit);
+        window.location.reload(true);
+      }
+    } catch (error) {
+      this.errorMessage = error.message;
+    }
+  }
+
   get movie() {
     return this.args.movie.data();
   }
